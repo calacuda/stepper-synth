c9b0fd49ee998b4226b53fb1c8855e6557a3121b
Eoghan West <eowest@gmail.com>
2025-02-22 20:23:43 -0500

    env filter gui visuals done

--- gui/stepper_synth/wt_env_menu.py
+++ gui/stepper_synth/wt_env_menu.py
@@ -28,28 +28,40 @@ def draw_env(pygame, screen, fonts, env, top, left, env_i):
     row_offset = h / 6
 
     display_things = (
-        ("A", env.atk, format_time(env.atk)),
+        (f"A-{env_i + 1}", env.atk, format_time(env.atk)),
         ("D", env.dcy, format_time(env.dcy)),
         ("S", env.sus, f"{round(env.sus * 100)}"),
         ("R", env.rel, format_time(env.rel)),
     )
 
-    print(env_i, env_sel, f"{X_I} => {X_I % 4}")
+    # print(env_i, env_sel, f"{X_I} => {X_I % 4}")
 
     for (i, (label, val, display)) in enumerate(display_things):
-        m_x = offset + col_w * i + left
+        x = offset + col_w * i + left
 
         y = row_offset + row_h * 0 + top
         sel = env_sel and X_I % 4 == i
         color = TEXT_COLOR_2 if not sel else PEACH
-        draw_text(screen, label, fonts[2], (m_x, y), color)
+        draw_text(screen, label, fonts[2], (x, y), color)
 
         # TODO: draw dial
+        y = row_offset + row_h * 1 + top
+        draw_dial(pygame, screen, x, y, val, sel, diameter=col_w / 2)
+
         # TODO: draw display
+        y = row_offset + row_h * 2 + top
+        draw_text(screen, display, fonts[1], (x, y), color)
 
 
 def draw_env_menu(pygame, screen, fonts, synth: StepperSynthState):
     envs = synth.adsr
+
+    # draw horizontal divider
+    pygame.draw.line(screen, GREEN, (0, SCREEN_CENTER[1]),
+                     (SCREEN_WIDTH, SCREEN_CENTER[1]), int(LINE_WIDTH / 2))
+    # draw vertical divider
+    pygame.draw.line(screen, GREEN, (SCREEN_CENTER[0], 0),
+                     (SCREEN_CENTER[0], SCREEN_WIDTH), int(LINE_WIDTH / 2))
 
     for i in range(4):
         top = (SCREEN_HEIGHT / 2) * (i // 2)
